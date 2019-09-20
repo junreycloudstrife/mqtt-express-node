cc3956f5a5ad73bafe52d9add872a7a6603215f9
junreycloudstrife <junrey.cloudstrife@gmail.com>
2019-09-20 15:45:33 +0800

    Incorporated expressjs

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-var http = require('http'),
-   fs = require('fs'),
-   // NEVER use a Sync function except at start-up!
-   index = fs.readFileSync(__dirname + '/index.html');
+var app = require('express')();
+var http = require('http').createServer(app);
 
-// Send index.html to all requests
-var app = http.createServer(function(req, res) {
-   res.writeHead(200, {'Content-Type': 'text/html'});
-   res.end(index);
+// app.get('/', function(req, res){
+//   res.send('<h1>Hello world</h1>');
+// });
+
+app.get('/', function(req, res){
+   res.sendFile(__dirname + '/index.html');
+ });
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
 
-app.listen(3000);
+
+// var http = require('http'),
+//    fs = require('fs'),
+//    // NEVER use a Sync function except at start-up!
+//    index = fs.readFileSync(__dirname + '/index.html');
+
+// // Send index.html to all requests
+// var app = http.createServer(function(req, res) {
+//    res.writeHead(200, {'Content-Type': 'text/html'});
+//    res.end(index);
+// });
+
+// app.listen(3000);
