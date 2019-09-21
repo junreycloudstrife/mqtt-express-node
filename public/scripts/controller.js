7a99ed81d913a32ff9cd99e552ce9fa0ed5faf0c
junreycloudstrife <junrey.cloudstrife@gmail.com>
2019-09-21 16:46:57 +0800

    Added display of message on the page

--- public/scripts/controller.js
+++ public/scripts/controller.js
@@ -1,17 +1,21 @@
 // basic functionalities
 function connectFunc(){
-  console.log("Connecting..");
+  // console.log("Connecting..");
+  displayMessage("Connecting..");  
   // client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
   client = mqtt.connect(document.getElementById('broker').value)
-  console.log(document.getElementById('broker').value);
+  // console.log(document.getElementById('broker').value);
+  displayMessage(document.getElementById('broker').value);
 
   client.on("connect", function(){
-    console.log("Successfully connected");
+    // console.log("Successfully connected");
+    displayMessage("Successfully connected");
   })
 
   client.on("message", function (topic, payload) {
     // console.log([topic, payload].join(": "));
-    console.log("Received { topic: " + topic + "; payload: " + payload + " }");
+    // console.log("Received { topic: " + topic + "; payload: " + payload + " }");
+    displayMessage("Received { topic: " + topic + "; payload: " + payload + " }");
     // client.end();
   })
 
@@ -20,8 +24,8 @@ function publishFunc(){
   // console.log("publish");
   // client.publish("mqtt/demo", "hello world!");
   client.publish(document.getElementById('pub-topic').value, document.getElementById('pub-payload').value)
-  console.log("Published { topic: " + document.getElementById('pub-topic').value
-  + "; payload: " + document.getElementById('pub-payload').value + " }");
+  // console.log("Published { topic: " + document.getElementById('pub-topic').value + "; payload: " + document.getElementById('pub-payload').value + " }");
+  displayMessage("Published { topic: " + document.getElementById('pub-topic').value + "; payload: " + document.getElementById('pub-payload').value + " }");
   // console.log(document.getElementById('pub-topic').value);
   // console.log(document.getElementById('pub-payload').value);
 }
@@ -30,9 +34,16 @@ function subscribeFunc(){
   // console.log("subscribe");
   // client.subscribe("mqtt/demo");
   client.subscribe(document.getElementById('sub-topic').value);
-  console.log("Subscribe { topic: " + document.getElementById('sub-topic').value + " }");
+  // console.log("Subscribe { topic: " + document.getElementById('sub-topic').value + " }");
+  displayMessage("Subscribe { topic: " + document.getElementById('sub-topic').value + " }")
 }
 
+function displayMessage(message) {
+  var node = document.createElement("li");
+  var textnode = document.createTextNode(message);
+  node.appendChild(textnode);
+  document.getElementById("messages").appendChild(node);
+}
 // // advance functionalities
 // client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
 // client.subscribe("mqtt/demo", function (err){
