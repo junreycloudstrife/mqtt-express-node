e89ba06303962ff683b3ecb7b4aa3687f0f466b4
junreycloudstrife <junrey.cloudstrife@gmail.com>
2019-09-21 15:18:37 +0800

    Incorporated expressjs with public directory

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
 var app = require('express')();
+var express = require('express');
 var http = require('http').createServer(app);
-
-// app.get('/', function(req, res){
-//   res.send('<h1>Hello world</h1>');
-// });
+// var mqtt = require('mqtt');
 
 app.get('/', function(req, res){
-   res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
  });
 
+app.use(express.static('public'));
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
-
-
-// var http = require('http'),
-//    fs = require('fs'),
-//    // NEVER use a Sync function except at start-up!
-//    index = fs.readFileSync(__dirname + '/index.html');
-
-// // Send index.html to all requests
-// var app = http.createServer(function(req, res) {
-//    res.writeHead(200, {'Content-Type': 'text/html'});
-//    res.end(index);
-// });
-
-// app.listen(3000);
